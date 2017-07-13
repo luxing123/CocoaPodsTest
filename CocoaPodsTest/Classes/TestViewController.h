289baa320b87735fb6d071ecaf21cc16247a257e
luxing <luxing123@qq.com>
2017-07-13 20:03:54 +0800

    commit

---
+++ CocoaPodsTest/Classes/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  Pods
+//
+//  Created by 111 on 2017/7/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ Example/Pods/Target Support Files/CocoaPodsTest/CocoaPodsTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CocoaPodsTest : NSObject
+@end
+@implementation PodsDummy_CocoaPodsTest
+@end
